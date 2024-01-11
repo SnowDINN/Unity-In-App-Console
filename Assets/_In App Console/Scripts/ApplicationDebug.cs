3248619050a9f4d7cfa104725f77717c24002208
DINN <dinn@richalien.net>
2024-01-11 18:27:32 +0900

    Update console system

--- Assets/_In App Console/Scripts/ApplicationDebug.cs
+++ Assets/_In App Console/Scripts/ApplicationDebug.cs
@@ -1,102 +1,119 @@
-using Anonymous.Systems;
+using System.Collections.Generic;
 using System.Linq;
+using Anonymous.Systems;
 using UnityEngine;
-using UnityEngine.Profiling;
+using UnityEngine.SceneManagement;
 
 namespace Anonymous
 {
-    public enum DebugOptions
-    {
-        DEBUG,
-        DEBUG_STATUS,
-    }
-    
-    public class ApplicationDebug
-    {
-        private const string ERROR_COMMAND = "<color=red>This command is error.</color>";
-        
-        private static GameObject go;
+	public class ApplicationDebug
+	{
+		private const string ERROR_COMMAND = "<color=red>The command you entered does not exist.</color>";
+		public const string PPK_PROFILER = "APPLICATION_PROFILER_ACTIVATOR";
+		public const string PPK_DEBUG = "APPLICATION_DEBUG_ACTIVATOR";
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        public static void RuntimeInitializeLoad()
-        {
-            StartedApplicationDebug();
-            
-            ApplicationDebugCommandSystem.AddCommand("Profiler", command =>
-            {
-                var split = command.Split(' ');
-                switch (split.Length)
-                {
-                    case 1:
-                    {
-                        var isOn = switchActivate(DebugOptions.DEBUG_STATUS);
-						
-                        Debug.Log("Profiler : " + $"{(isOn ? "on" : "off")}");
-                        break;
-                    }
-                    default:
-                    {
-                        Debug.Log(ERROR_COMMAND);
-                        break;
-                    }
-                }
-            });
-            ApplicationDebugCommandSystem.AddComment("Profiler", "Device Profiling Un/Activate");
-        }
-        
-        public static void StartedApplicationDebug()
-        {
-            if (go != null)
-                return;
-            
-            go = Object.Instantiate(Resources.Load<GameObject>("Debug"));
-            Object.DontDestroyOnLoad(go);
-            onActivate(DebugOptions.DEBUG);
+		private static Canvas canvas;
+		private static GameObject go;
 
-            var canvas = go.GetComponent<Canvas>();
-            canvas.worldCamera = Camera.main;
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+		public static void RuntimeInitializeLoad()
+		{
+			StartedApplicationDebug();
 
-            var components = go.GetComponentsInChildren<IApplicationDebugSystems>(true).ToArray();
-            foreach (var component in components)
-                component.Setup();
-        }
+			ApplicationDebugCommandSystem.AddCommand("Profiler")
+				.AddComment("It shows the performance status of your device.")
+				.AddParameters(new List<string> { "on", "off" })
+				.AddParameters(new List<string> { "active", "unactive" })
+				.AddExecute(command =>
+				{
+					var split = command.Split(' ');
+					switch (split.Length)
+					{
+						case 1:
+						{
+							var isOn = SwitchActivate();
 
-        public static void EndedApplicationDebug()
-        {
-            if (go == null)
-                return;
+							Debug.Log("Profiler : " + $"{(isOn ? "on" : "off")}");
+							break;
+						}
+						default:
+						{
+							Debug.Log(ERROR_COMMAND);
+							break;
+						}
+					}
+				});
+		}
 
-            Object.Destroy(go);
-            offActivate(DebugOptions.DEBUG);
-            
-            var components = go.GetComponentsInChildren<IApplicationDebugSystems>(true).ToArray();
-            foreach (var component in components)
-                component.Dispose();
-        }
+		public static void StartedApplicationDebug()
+		{
+			if (go != null)
+				return;
 
-        public static void onActivate(DebugOptions type)
-        {
-            PlayerPrefs.SetInt($"{type}", 0);
-        }
-        
-        public static void offActivate(DebugOptions type)
-        {
-            PlayerPrefs.DeleteKey($"{type}");
-        }
+			go = Object.Instantiate(Resources.Load<GameObject>("Debug"));
+			go.name = "DEBUGGER";
+			SetWorldCamera();
 
-        public static bool isActivate(DebugOptions type)
-        {
-            return PlayerPrefs.HasKey($"{type}");
-        }
-        
-        public static bool switchActivate(DebugOptions type)
-        {
-            if (PlayerPrefs.HasKey($"{type}"))
-                PlayerPrefs.DeleteKey($"{type}");
-            else
-                PlayerPrefs.SetInt($"{type}", 0);
-            
-            return PlayerPrefs.HasKey($"{type}");
-        }
-    }
+			Object.DontDestroyOnLoad(go);
+			OnActivate();
+
+			var components = go.GetComponentsInChildren<IApplicationDebugSystems>(true).ToArray();
+			foreach (var component in components)
+				component.Setup();
+			
+			SceneManager.activeSceneChanged += OnChangeScene;
+		}
+
+		public static void EndedApplicationDebug()
+		{
+			if (go == null)
+				return;
+
+			Object.Destroy(go);
+			OffActivate();
+
+			var components = go.GetComponentsInChildren<IApplicationDebugSystems>(true).ToArray();
+			foreach (var component in components)
+				component.Dispose();
+			
+			SceneManager.activeSceneChanged -= OnChangeScene;
+		}
+
+		public static bool isActivate(string key)
+		{
+			return PlayerPrefs.HasKey(key);
+		}
+
+		public static void OnActivate()
+		{
+			PlayerPrefs.SetInt(PPK_DEBUG, 0);
+		}
+
+		public static void OffActivate()
+		{
+			PlayerPrefs.DeleteKey(PPK_DEBUG);
+		}
+
+		public static bool SwitchActivate()
+		{
+			if (PlayerPrefs.HasKey(PPK_PROFILER))
+				PlayerPrefs.DeleteKey(PPK_PROFILER);
+			else
+				PlayerPrefs.SetInt(PPK_PROFILER, 0);
+
+			return PlayerPrefs.HasKey(PPK_PROFILER);
+		}
+		
+		private static void OnChangeScene(Scene arg0, Scene arg1)
+		{
+			SetWorldCamera();
+		}
+
+		private static void SetWorldCamera()
+		{
+			if (canvas == null)
+				canvas = go.GetComponent<Canvas>();
+			canvas.worldCamera = Camera.main;
+		}
+	}
 }
